08fd83625f45528e782afe50dfd57fbdb165cfa8
Camille Sierra <camillemsierra@gmail.com>
2018-08-02 09:43:01 -0400

    I have burgers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,7 +9,7 @@ router.get("/", function (req, res) {
         var hbsObject = {
             burgers: data
         }
-        res.render('index', {hbsObject});
+        res.render('index', hbsObject);
     });
 
 });
